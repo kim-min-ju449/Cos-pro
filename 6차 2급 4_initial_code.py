f898deef3ebbe6d3e9b5052698ea8b9b51363ed2
kim-min-ju449 <77184157+kim-min-ju449@users.noreply.github.com>
2022-01-13 15:50:27 +0900

    2022. 1.13 커밋

---
+++ 6차 2급 4_initial_code.py
@@ -0,0 +1,38 @@
+#다음과 같이 import를 사용할 수 있습니다.
+#import math
+
+def solution(cards):
+    #여기에 코드를 작성해주세요.
+
+    answer = 0
+    counter = [0,0,0]
+    total = 0
+    for card in cards:
+        color = card[0]
+        number = card[1]
+        if color =="red":
+            counter[0]+=1
+        elif color == "black":
+            counter[1] +=1
+        elif color == "blue":
+            counter[2] +=1
+
+        total +=int(number)
+    weight = max(counter)
+    answer = total * weight
+
+
+    return answer
+
+#아래는 테스트케이스 출력을 해보기 위한 코드입니다.
+cards1 = [["blue", "2"], ["red", "5"], ["black", "3"]]
+ret1 = solution(cards1)
+
+#[실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+print("solution 함수의 반환 값은", ret1, "입니다.")
+
+cards2 = [["blue", "2"], ["blue", "5"], ["black", "3"]]
+ret2 = solution(cards2)
+
+#[실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+print("solution 함수의 반환 값은", ret2, "입니다.")
